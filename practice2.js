77aa9deae9f7ae5cad769e35c96e21d38d5fa7ce
kazi shakerodin <shakerodink@gmail.com>
2022-10-25 17:05:14 +0530

    first commit

---
+++ practice2.js
@@ -0,0 +1,8 @@
+function myFunction() 
+{ 
+    document.getElementById("demo2").innerHTML = "dynamic paragraph created by javascript on click event";
+    document.getElementById("demo2").style.background = "red";
+    document.getElementById("demo2").style.fontSize = "100px"; 
+    document.getElementById("demo2").style.border= "10px solid yellow"; 
+    document.getElementById("demo2").style.color= "green"; 
+    };
